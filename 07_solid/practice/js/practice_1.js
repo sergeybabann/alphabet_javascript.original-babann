38e40a8803e43f0767df472d817f8a1209677695
sergeybabann <sergeybabann@yandex.ru>
2024-12-06 19:29:12 +0300

    lesson complete: 07. Понятие объектно-ориентированного программирования

--- 07_solid/practice/js/practice_1.js
+++ 07_solid/practice/js/practice_1.js
@@ -1,1 +1,23 @@
 'strict mode'
+
+// объект user
+
+const User = {
+  name: 'Alex',
+  surname: 'Nav',
+  age: 49,
+}
+
+const newUser = new Object()
+
+const litUser = {}
+
+const oldUser = Object.create(User)
+
+console.log(User.name)
+console.log(newUser)
+console.log(litUser)
+console.log(oldUser.name)
+
+oldUser.name = 'Serg'
+console.log(oldUser.name)
